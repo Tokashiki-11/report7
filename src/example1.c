8beba766a908059952e64eb68360d6c196419305
Tokashiki Anzu <e245744@ie.u-ryukyu.ac.jp>
2025-01-26 17:00:47 +0900

    Wrong code (ascending order)

--- src/example1.c
+++ src/example1.c
@@ -12,7 +12,9 @@ int main(){
     for (int i=0; i<SIZE; i++){ //バブルソート
         for (int j=0; j<SIZE; j++){
             if (scores[i] < scores[j]){
-
+                int temp = scores[i];
+                scores[i] = scores[j];
+                scores[j] = temp;
             }
         }
     }
