9545d9001fdc76e3bc8b365d067fb841d7b7aa75
Tokashiki Anzu <e245744@ie.u-ryukyu.ac.jp>
2025-01-26 15:34:33 +0900

    Under consideration (write something anyway) Part2

--- src/example1.c
+++ src/example1.c
@@ -3,7 +3,9 @@
 int main(){
     const int SIZE = 5;
     int scores[SIZE] = {0, 60, 70, 100, 90};
+    printf("scores = ");
     for (int i=0; i<SIZE; i++){
-        
+        printf("%d ", scores[i]);
     }
+    
 }
