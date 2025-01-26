987cbac01391984b3d613037d72319da71aa6154
Tokashiki Anzu <e245744@ie.u-ryukyu.ac.jp>
2025-01-26 15:46:12 +0900

    Trying to implement code to reorder with bubble sort

--- src/example1.c
+++ src/example1.c
@@ -8,4 +8,11 @@ int main(){
         printf("%d ", scores[i]);
     }
     
+    for (int i=0; i<SIZE; i++){ //バブルソート
+        for (int j=0; j<SIZE; j++){
+            if (scores[i] < scores[j]){
+
+            }
+        }
+    }
 }
