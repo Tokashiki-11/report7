88e64bb9dc7a5f6e17901327e2e18a54a5ba2a2a
Tokashiki Anzu <e245744@ie.u-ryukyu.ac.jp>
2025-01-26 19:39:58 +0900

    Create sortDescending function

--- src/example1.c
+++ src/example1.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void sortDescending(int array[], int size);
+void sortDescending(int array[], int size){
+    for (int i=0; i<size; i++){ //バブルソート
+        for (int j=0; j<size; j++){
+            if (array[i] > array[j]){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
 int main(){
     const int SIZE = 5;
     int scores[SIZE] = {0, 60, 70, 100, 90};
@@ -8,16 +21,6 @@ int main(){
         printf("%d ", scores[i]);
     }
     printf("\n");
-    
-    for (int i=0; i<SIZE; i++){ //バブルソート
-        for (int j=0; j<SIZE; j++){
-            if (scores[i] > scores[j]){
-                int temp = scores[i];
-                scores[i] = scores[j];
-                scores[j] = temp;
-            }
-        }
-    }
 
     printf("result = ");
     for (int i=0; i<SIZE; i++){
